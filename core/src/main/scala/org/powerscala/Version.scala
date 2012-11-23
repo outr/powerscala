62fffd9c9815214f33f91dd43144ef9095dd3da1
Matt Hicks <matt@outr.com>
2012-11-23 15:38:08 -0600

    Added compare support to Version.

--- core/src/main/scala/org/powerscala/Version.scala
+++ core/src/main/scala/org/powerscala/Version.scala
@@ -5,6 +5,18 @@ package org.powerscala
  *
  * @author Matt Hicks <matt@outr.com>
  */
-case class Version(major: Int = 1, minor: Int = 0, maintenance: Int = 0, build: Int = 0) {
+case class Version(major: Int = 1, minor: Int = 0, maintenance: Int = 0, build: Int = 0) extends Ordered[Version] {
   override def toString = "%s.%s.%s.%s".format(major, minor, maintenance, build)
+
+  def compare(that: Version) = if (major != that.major) {
+    major.compare(that.major)
+  } else if (minor != that.minor) {
+    minor.compare(that.minor)
+  } else if (maintenance != that.maintenance) {
+    maintenance.compare(that.maintenance)
+  } else if (build != that.build) {
+    build.compare(that.build)
+  } else {
+    0
+  }
 }
