2493ae0844f50d8c9ad9f7987fef7476febe36ff
darkfrog26 <matt@outr.com>
2013-03-18 12:17:13 -0500

    Updated to 1.5.1-SNAPSHOT.

--- project/PowerScalaBuild.scala
+++ project/PowerScalaBuild.scala
@@ -5,7 +5,7 @@ import Dependencies._
 
 object PowerScalaBuild extends Build {
   val baseSettings = Defaults.defaultSettings ++ Seq(
-    version := "1.5.0-SNAPSHOT",
+    version := "1.5.1-SNAPSHOT",
     organization := "org.powerscala",
     scalaVersion := "2.9.2",
     libraryDependencies ++= Seq(
