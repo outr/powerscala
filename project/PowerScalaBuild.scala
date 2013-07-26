e4ef0bff0fb4ca8b3c2690f45e8025bcb0cc4f6c
Matt Hicks <matt@outr.com>
2013-07-26 12:21:37 -0500

    Release of 1.6.0

--- project/PowerScalaBuild.scala
+++ project/PowerScalaBuild.scala
@@ -6,7 +6,7 @@ import Dependencies._
 
 object PowerScalaBuild extends Build {
   val baseSettings = Defaults.defaultSettings ++ Seq(
-    version := "1.6.0-SNAPSHOT",
+    version := "1.6.0",
     organization := "org.powerscala",
     scalaVersion := "2.10.1",
     libraryDependencies ++= Seq(
