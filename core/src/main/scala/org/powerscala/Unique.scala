5b719867d23b9ef55ebdd351f01833a4f58a41c7
Matt Hicks <matt@outr.com>
2014-01-27 10:40:41 -0600

    Updated Unique to no longer use UUID.randomUUID() as the performance is poor. Instead using constructor with random longs.

--- core/src/main/scala/org/powerscala/Unique.scala
+++ core/src/main/scala/org/powerscala/Unique.scala
@@ -1,13 +1,19 @@
 package org.powerscala
 
 import java.util.UUID
+import scala.util.Random
+import java.util.concurrent.ThreadLocalRandom
+import scala.annotation.tailrec
 
 /**
  * @author Matt Hicks <matt@outr.com>
  */
 object Unique {
-  def apply(): String = UUID.randomUUID().toString.replace("-", "") match {
-    case s if s.charAt(0).isDigit => apply()
-    case s => s
+  private val r = new Random(ThreadLocalRandom.current())
+
+  @tailrec
+  final def apply(): String = new UUID(r.nextLong(), r.nextLong()).toString.replace("-", "") match {
+    case s if !s.charAt(0).isDigit => s
+    case s => apply()
   }
 }
