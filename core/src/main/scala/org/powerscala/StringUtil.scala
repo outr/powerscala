2a5775ab27795b5f4eeccc0adda79e73155dc2e3
Matt Hicks <matt@outr.com>
2014-05-17 19:07:33 -0500

    Created StringUtil to offer some standard functionality.

---
+++ core/src/main/scala/org/powerscala/StringUtil.scala
@@ -0,0 +1,23 @@
+package org.powerscala
+
+import org.powerscala.reflect.CaseValue
+
+/**
+ * @author Matt Hicks <matt@outr.com>
+ */
+object StringUtil {
+  /**
+   * Converts space and dash separated to camel-case
+   */
+  def toCamelCase(name: String) = "[- ]([a-zA-Z0-9])".r.replaceAllIn(name, m => m.group(1).toUpperCase)
+
+  /**
+   * Converts camelCase to dash-separated.
+   */
+  def fromCamelCase(name: String) = "([A-Z])".r.replaceAllIn(name, m => "-" + m.group(0).toLowerCase)
+
+  /**
+   * Generates a human readable label for this name with proper capitalization.
+   */
+  def generateLabel(name: String) = CaseValue.generateLabel(name)
+}
