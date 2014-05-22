86a7d9e1701689be488ee452eb69ed90662dc3f7
Matt Hicks <matt@outr.com>
2014-05-21 21:00:45 -0500

    Bug fix to Unique causing duplicate random generation between executions.

--- core/src/main/scala/org/powerscala/Unique.scala
+++ core/src/main/scala/org/powerscala/Unique.scala
@@ -1,7 +1,6 @@
 package org.powerscala
 
 import java.util.UUID
-import scala.util.Random
 import java.util.concurrent.ThreadLocalRandom
 import scala.annotation.tailrec
 
@@ -9,7 +8,7 @@ import scala.annotation.tailrec
  * @author Matt Hicks <matt@outr.com>
  */
 object Unique {
-  private val r = new Random(ThreadLocalRandom.current())
+  private def r = ThreadLocalRandom.current()
 
   @tailrec
   final def apply(): String = new UUID(r.nextLong(), r.nextLong()).toString.replace("-", "") match {
