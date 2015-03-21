411a73337a80a1ebdfc7340e995301a1dd551e59
Matt Hicks <matt@outr.com>
2015-03-20 21:35:21 -0500

    Synchronized modification to ListProperty to avoid thread-safety issues.

--- property/src/main/scala/org/powerscala/property/ListProperty.scala
+++ property/src/main/scala/org/powerscala/property/ListProperty.scala
@@ -4,10 +4,16 @@ package org.powerscala.property
  * @author Matt Hicks <matt@outr.com>
  */
 trait ListProperty[T] extends Property[List[T]] {
-  def +=(t: T) = this := (t :: value.reverse).reverse
-  def -=(t: T) = this := value.filterNot(p => p == t)
+  def +=(t: T) = synchronized {
+    this := (t :: value.reverse).reverse
+  }
+  def -=(t: T) = synchronized {
+    this := value.filterNot(p => p == t)
+  }
   def isEmpty = value.isEmpty
   def nonEmpty = value.nonEmpty
-  def ++=(seq: Seq[T]) = this := (seq.toList.reverse ::: value.reverse).reverse
+  def ++=(seq: Seq[T]) = synchronized {
+    this := (seq.toList.reverse ::: value.reverse).reverse
+  }
   def contains(t: T) = value.contains(t)
 }
