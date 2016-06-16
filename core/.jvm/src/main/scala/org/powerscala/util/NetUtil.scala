05a49220e2e64425a556710753a0751616fd6eb9
Matt Hicks <matt@outr.com>
2016-06-16 18:43:35 -0500

    Updated NetUtil

--- core/.jvm/src/main/scala/org/powerscala/util/NetUtil.scala
+++ core/.jvm/src/main/scala/org/powerscala/util/NetUtil.scala
@@ -1,5 +1,34 @@
 package org.powerscala.util
 
-object NetUtil {
+import java.net.ServerSocket
 
+import scala.annotation.tailrec
+
+object NetUtil {
+  @tailrec
+  final def nextTCPPort(from: Int = 1024, to: Int = Int.MaxValue): Option[Int] = {
+    if (isTCPPortFree(from)) {
+      Some(from)
+    } else if (from == to) {
+      None
+    } else {
+      nextTCPPort(from + 1, to)
+    }
+  }
+
+  def availableTCPPort(): Int = {
+    val ss = new ServerSocket(0)
+    try {
+      ss.getLocalPort
+    } finally {
+      ss.close()
+    }
+  }
+
+  def isTCPPortFree(port: Int): Boolean = try {
+    new ServerSocket(port).close()
+    true
+  } catch {
+    case t: Throwable => false
+  }
 }
